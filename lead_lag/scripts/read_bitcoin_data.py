4cdf1c93a23e2d17ece84b3989b449b0e85bcc97
Satyajeet Pal <psatyajeet@gmail.com>
2020-07-24 17:11:15 +0000

    Update read_bitcoin_data functions

--- lead_lag/scripts/read_bitcoin_data.py
+++ lead_lag/scripts/read_bitcoin_data.py
@@ -9,6 +9,7 @@ warnings.filterwarnings('ignore', message='numpy.ufunc size changed')
 def read_small_data(small_filename):
     # print(f'Reading {small_filename}')
     d = pd.read_csv(small_filename, parse_dates=True, index_col=0)
+    # returns data in centiseconds
     d['timestamp'] = d.index.values.astype(np.int64) // 10 ** 7
     return d
 
@@ -18,3 +19,21 @@ def bitcoin_data(exchange_1_small_data_file, exchange_2_small_data_file):
     exchange_2 = read_small_data(exchange_2_small_data_file)
 
     return exchange_1[['timestamp', 'last']].values, exchange_2[['timestamp', 'last']].values
+
+def bitcoin_data_v2(exchange_1_small_data_file, exchange_2_small_data_file):
+    exchange_1 = read_small_data(exchange_1_small_data_file)
+    exchange_2 = read_small_data(exchange_2_small_data_file)
+
+    exchange_1_columns = list(exchange_1.columns)
+    exchange_2_columns = list(exchange_2.columns)
+
+    return exchange_1[[exchange_1_columns[1], exchange_1_columns[0]]].values, exchange_2[[exchange_2_columns[1], exchange_2_columns[0]]].values
+
+def bitcoin_data_v2_filtered(exchange_1_small_data_file, exchange_2_small_data_file, start_date, end_date):
+    exchange_1 = read_small_data(exchange_1_small_data_file).between_time(start_date.time(), end_date.time())
+    exchange_2 = read_small_data(exchange_2_small_data_file).between_time(start_date.time(), end_date.time())
+
+    exchange_1_columns = list(exchange_1.columns)
+    exchange_2_columns = list(exchange_2.columns)
+
+    return exchange_1[[exchange_1_columns[1], exchange_1_columns[0]]].values, exchange_2[[exchange_2_columns[1], exchange_2_columns[0]]].values
